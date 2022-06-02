58f79d7226598def4970acc21b7088bf4bcc9d21
936lady <queenkleome@gmail.com>
2022-06-02 16:01:05 -0700

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	pwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
