a56a616770e08f5096618cd90dcb1a912597ba09
936lady <queenkleome@gmail.com>
2022-06-06 16:54:48 -0700

    tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - 8. UNIX is basically
+ *a simple operating system,
+ *but you have to be a genius
+ *to understand the simplicity
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
+}
