02f884d2dbfb5e46f6d499387cf82bca5f5b99e5
936lady <queenkleome@gmail.com>
2022-06-06 17:42:21 -0700

    tasks

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,22 +8,3 @@
  */
 int main(void)
 {
-	char d = '0';
-
-	while (d <= '9')
-	{
-		putchar(d);
-		d++;
-	}
-
-	char c = 'a';
-
-	while (c <= 'f')
-	{
-		putchar(c);
-		c++;
-	}
-
-	putchar('\n');
-	return (0);
-}
