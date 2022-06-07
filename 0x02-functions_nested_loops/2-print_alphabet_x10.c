49d23a3d688b0e32eb62fcb3f7b5d50000c46e4a
936lady <queenkleome@gmail.com>
2022-06-07 15:00:13 -0700

    tasks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - task 2.
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int x;
+
+	for (x = 0; x < 10 ; x++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+		_putchar(a);
+		}
+		_putchar('\n');
+}
+
