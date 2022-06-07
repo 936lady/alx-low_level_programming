a54cdda517967cde8e5096f1a28d57b2ea37138f
936lady <queenkleome@gmail.com>
2022-06-06 17:35:16 -0700

    tasks

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - 8. Hexadecimal
+ * lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d;
+	char l;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
+}
