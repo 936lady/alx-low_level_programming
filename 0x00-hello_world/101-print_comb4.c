4d4c1efe196e8e4db79ab6819f3fddef7afd5292
936lady <queenkleome@gmail.com>
2022-06-06 17:26:45 -0700

    tasks

---
+++ 0x00-hello_world/101-print_comb4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main - 11. The success combination in business is:
+ *   Do what you do better... and:
+ *    do more of what you do...
+ * Return - 0 
+ **/
+int main(void)
+{
+	int /* not sure...will retry again*/
