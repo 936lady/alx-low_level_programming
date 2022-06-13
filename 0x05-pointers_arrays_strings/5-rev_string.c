9dc5368d3fee3d44ea3f8a3efbf6aa34eb2e9366
936lady <queenkleome@gmail.com>
2022-06-13 09:39:05 -0700

    tasks

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -32,18 +32,3 @@ void rev_string(char *s)
 	}
 }
 
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
-}
