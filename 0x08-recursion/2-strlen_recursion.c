5cd090a93164c265548f6371f181766d532a009e
936lady <queenkleome@gmail.com>
2022-06-22 16:30:38 -0700

    tasks

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,10 @@
 int _strlen_recursion(char *s)
 {
 	int len;
-	len = 0 
+	len = 0;
 		if (*s == '\0')
 			return (0);
-	len = 1 + _strlen_recursion(s + 1)
+	len = 1 + _strlen_recursion(s + 1);
 		return (len);
 ]
 
