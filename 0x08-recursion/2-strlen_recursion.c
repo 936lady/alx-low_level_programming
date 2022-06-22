07e18d7bec9b2e48f53ed1298a70efdf5a53fcae
936lady <queenkleome@gmail.com>
2022-06-22 16:28:39 -0700

    tasks

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Write a function that returns the length of a string.
+ * 2. Dreams feel real while we're in them.
+ * It's only when we wake up that we realize
+ * something was actually strange
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+	len = 0 
+		if (*s == '\0')
+			return (0);
+	len = 1 + _strlen_recursion(s + 1)
+		return (len);
+]
+
