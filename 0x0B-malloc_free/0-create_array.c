3a84de068e0f03a5f5108227d3f1855cf4fabdd5
936lady <queenkleome@gmail.com>
2022-06-28 04:46:33 -0700

    tasks

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_array - Write a function that creates an array of chars,
+ * and initializes it with a specific char
+ * 0. Float like a butterfly, sting like a bee
+ * @size: size of array
+ * @c: character
+ * Return: pointer to array or Null if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	a = malloc(size * sizeof(*a));
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		a[i] = c;
+
+	return (a);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strdup - copy a string using malloc
+ * 1. The woman who has no imagination has no wings
+ * @str: string to duplicate
+ * Return: Pointer
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	int i, c;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	a = malloc(i * sizeof(*a) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (c = 0; c < i; c++)
+		a[c] = str[c];
+	a[c] = '\0';
+
+	return (a);
+}
